17457af47f3df3ac54bef1fe58c02a921255a21f
KierPalin <45743174+KierPalin@users.noreply.github.com>
2025-01-26 07:27:42 +0000

    source/drivers/ST7735.cpp: sendColorsStep bpp changes

--- source/drivers/ST7735.cpp
+++ source/drivers/ST7735.cpp
@@ -314,29 +314,14 @@ int ST7735::sendIndexedImage(const uint8_t *src, unsigned width,
   if (!work) {
     work = new ST7735WorkBuffer;
     memset(work, 0, sizeof(*work));
-    // if (double16)
-    // for (int i = 0; i < 32; ++i) {
-    // uint16_t e = ENC16(i, i, i);
-    // work->expPalette[i] = e | (e << 16);
-    // work->expPalette[i] = palette[i];
-    // }
-    // else
-    // for (int i = 0; i < 256; ++i)
-    //   work->expPalette[i] = 0x111110 * (i & 0x1f) | (0x110000 * (i >> 5));
-
-    for (int i = 0; i < 32; ++i) {
-      uint32_t color = palette[i];
-      uint8_t r = (color >> 16) & 0xFF;
-      uint8_t g = (color >> 8) & 0xFF;
-      uint8_t b = color & 0xFF;
-
-      work->expPalette[i] = (r << 16) | (g << 8) | b;
-    }
-
-    for (int i = 32; i < 256; ++i) {
-      work->expPalette[i] = 0x000000;
-    }
-
+    if (double16)
+      for (int i = 0; i < 32; ++i) {
+        uint16_t e = ENC16(i, i, i);
+        work->expPalette[i] = e | (e << 16);
+      }
+    else
+      for (int i = 0; i < 256; ++i)
+        work->expPalette[i] = 0x1011 * (i & 0xf) | (0x110100 * (i >> 4));
     EventModel::defaultEventBus->listen(DEVICE_ID_DISPLAY, 100, this,
                                         &ST7735::sendDone);
   }
