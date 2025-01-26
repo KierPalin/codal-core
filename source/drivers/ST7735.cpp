0a27a812d1964c9476d54d77fc3b875b2658d1f5
KierPalin <45743174+KierPalin@users.noreply.github.com>
2025-01-26 04:16:56 +0000

    source/drivers/ST7735.cpp: sendColorsStep bpp changes

--- source/drivers/ST7735.cpp
+++ source/drivers/ST7735.cpp
@@ -66,7 +66,7 @@ namespace codal {
 
 ST7735::ST7735(ScreenIO &io, Pin &cs, Pin &dc)
     : io(io), cs(&cs), dc(&dc), work(NULL) {
-  double16 = false;
+  double16 = true;
   inSleepMode = false;
 }
 
