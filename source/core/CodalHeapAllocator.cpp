523f180948c23c0f7b383c9d786eb35f31ecfb7b
KierPalin <45743174+KierPalin@users.noreply.github.com>
2025-09-23 14:39:13 +0100

    Undoing CodalHeapAllocator.cpp changes

--- source/core/CodalHeapAllocator.cpp
+++ source/core/CodalHeapAllocator.cpp
@@ -63,7 +63,7 @@ using namespace codal;
 HeapDefinition heap[DEVICE_MAXIMUM_HEAPS] = { };
 uint8_t heap_count = 0;
 
-// #if (CODAL_DEBUG >= CODAL_DEBUG_HEAP)
+#if (CODAL_DEBUG >= CODAL_DEBUG_HEAP)
 // Diplays a usage summary about a given heap...
 void device_heap_print(HeapDefinition &heap)
 {
@@ -106,9 +106,6 @@ void device_heap_print(HeapDefinition &heap)
     target_enable_irq();
 
     DMESG("\n");
-
-    volatile unsigned int total_mb_free = totalFreeBlock*DEVICE_HEAP_BLOCK_SIZE; 
-    volatile unsigned int total_mb_used = totalUsedBlock*DEVICE_HEAP_BLOCK_SIZE; 
     DMESG("mb_total_free : %d", totalFreeBlock*DEVICE_HEAP_BLOCK_SIZE);
     DMESG("mb_total_used : %d", totalUsedBlock*DEVICE_HEAP_BLOCK_SIZE);
 }
@@ -122,7 +119,7 @@ void device_heap_print()
         device_heap_print(heap[i]);
     }
 }
-// #endif
+#endif
 
 /**
   * Create and initialise a given memory region as for heap storage.
@@ -287,7 +284,6 @@ void *device_malloc_in(size_t size, HeapDefinition &heap)
 REAL_TIME_FUNC
 void* device_malloc (size_t size)
 {
-    device_heap_print();
     static uint8_t initialised = 0;
     void *p;
 
